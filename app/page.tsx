c3422d568c808cb3b852427136f50b39c543d07b
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-05-02 15:05:05 +0500

    Routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Hello from '@/app/components/hello';
 
 const Home = () => {
     return (
         <main>
             <div className={'text-5xl underline'}>Home</div>
-            <Hello />
         </main>
     );
 };
