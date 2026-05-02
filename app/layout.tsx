24fd4f712641a7bb4ea93fc47b893783295d3cbc
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-05-02 15:17:49 +0500

    Route Groups

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { ReactNode } from 'react';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -20,14 +21,16 @@ export const metadata: Metadata = {
 export default function RootLayout({
     children,
 }: Readonly<{
-    children: React.ReactNode;
+    children: ReactNode;
 }>) {
     return (
         <html
             lang='en'
             className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
         >
-            <body className='min-h-full flex flex-col'>{children}</body>
+            <body className='min-h-full flex flex-col'>
+                {children}
+            </body>
         </html>
     );
 }
