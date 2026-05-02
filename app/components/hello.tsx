c2ba68ce56c52a516cc746d013aa85bab9493b21
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-05-02 14:49:18 +0500

    server and client components

---
+++ app/components/hello.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+const Hello = () => {
+    console.log('Hello!');
+    return <div>Hello</div>;
+};
+
+export default Hello;
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Hello from '@/app/components/hello';
+
+const Home = () => {
+    return (
+        <main>
+            <div className={'text-5xl underline'}>Home</div>
+            <Hello />
+        </main>
+    );
+};
+
+export default Home;
